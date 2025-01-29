3d5f5dc0d1410ff0749a89f2ac4d3b69a962f671
loks0n <22452787+loks0n@users.noreply.github.com>
2025-01-29 08:04:38 +0000

    chore: bump versions

--- Appwrite/Client.cs
+++ Appwrite/Client.cs
@@ -69,11 +69,11 @@ namespace Appwrite
             _headers = new Dictionary<string, string>()
             {
                 { "content-type", "application/json" },
-                { "user-agent" , "AppwriteDotNetSDK/0.11.1 (${Environment.OSVersion.Platform}; ${Environment.OSVersion.VersionString})"},
+                { "user-agent" , "AppwriteDotNetSDK/0.12.0 (${Environment.OSVersion.Platform}; ${Environment.OSVersion.VersionString})"},
                 { "x-sdk-name", ".NET" },
                 { "x-sdk-platform", "server" },
                 { "x-sdk-language", "dotnet" },
-                { "x-sdk-version", "0.11.1"},                { "X-Appwrite-Response-Format", "1.6.0" }
+                { "x-sdk-version", "0.12.0"},                { "X-Appwrite-Response-Format", "1.6.0" }
             };
 
             _config = new Dictionary<string, string>();
