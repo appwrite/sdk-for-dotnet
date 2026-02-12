2394b99c31e971559a7cdd239f6340f784b56d76
root <chiragaggarwal5k@gmail.com>
2026-02-12 10:59:39 +0000

    remove blacklist enums

--- Appwrite/Enums/OAuthProvider.cs
+++ Appwrite/Enums/OAuthProvider.cs
@@ -50,7 +50,5 @@ namespace Appwrite.Enums
         public static OAuthProvider Yandex => new OAuthProvider("yandex");
         public static OAuthProvider Zoho => new OAuthProvider("zoho");
         public static OAuthProvider Zoom => new OAuthProvider("zoom");
-        public static OAuthProvider GithubImagine => new OAuthProvider("githubImagine");
-        public static OAuthProvider GoogleImagine => new OAuthProvider("googleImagine");
     }
 }
